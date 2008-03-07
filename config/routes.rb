e9d66309c17c7229a403c2e0729963838859f999
ed <ed@493adb5e-7a96-4b8c-87dc-e541e2512a3b>
2008-03-07 14:57:51 +0000

    adding authentication
    git-svn-id: svn+ssh://majakari.net/trackmystuff@142 493adb5e-7a96-4b8c-87dc-e541e2512a3b

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :locations
+  map.resources :users
 
+  map.resource :session
+  map.resource :session
+
+  map.resources :locations
   map.resources :events
 
   # The priority is based upon order of creation: first created -> highest priority.
