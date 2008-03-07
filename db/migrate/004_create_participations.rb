9049cfb139f2d4d3c9b88ee4e01120ad77cbfb69
ed <ed@493adb5e-7a96-4b8c-87dc-e541e2512a3b>
2008-03-07 14:58:35 +0000

    git-svn-id: svn+ssh://majakari.net/trackmystuff@144 493adb5e-7a96-4b8c-87dc-e541e2512a3b

---
+++ db/migrate/004_create_participations.rb
@@ -0,0 +1,12 @@
+class CreateParticipations < ActiveRecord::Migration
+  def self.up
+    create_table :participations do |t|
+      t.integer :user_id
+      t.integer :event_id
+    end
+  end
+
+  def self.down
+    drop_table :participations
+  end
+end
