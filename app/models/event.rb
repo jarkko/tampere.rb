44fef6dd51b8e57f3ab1dd8df31849f63cc1df74
ed <ed@493adb5e-7a96-4b8c-87dc-e541e2512a3b>
2008-03-11 07:19:27 +0000

    event dates must be unique
    git-svn-id: svn+ssh://majakari.net/trackmystuff@157 493adb5e-7a96-4b8c-87dc-e541e2512a3b

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :location
+  
+  validates_uniqueness_of :date
 
   def self.find_recent(count)
     self.find(:all, :order => 'date DESC', :limit => count)
