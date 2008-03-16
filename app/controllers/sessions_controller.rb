0145f07cc3fcffba52349f42e233b5043ff77340
ed <ed@493adb5e-7a96-4b8c-87dc-e541e2512a3b>
2008-03-16 13:54:22 +0000

    git-svn-id: svn+ssh://majakari.net/tampere.rb/trunk@188 493adb5e-7a96-4b8c-87dc-e541e2512a3b

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,9 @@ class SessionsController < ApplicationController
   def create
     self.current_user = User.authenticate(params[:login], params[:password])
     if logged_in?
-      #if params[:remember_me] == "1"
-        self.current_user.remember_me
-        cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
-      #end
+      self.current_user.remember_me
+      cookies[:auth_token] = { :value => self.current_user.remember_token,
+        :expires => self.current_user.remember_token_expires_at }
       redirect_back_or_default(DEFAULT_URL)
       flash[:notice] = "Sisäänkirjautuminen onnistui"
     else
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,69 +5,79 @@ require File.dirname(__FILE__) + '/../spec_helper'
 include AuthenticatedTestHelper
 
 describe SessionsController do
-  fixtures :users
-
   it 'logins and redirects' do
-    post :create, :login => 'quentin', :password => 'test'
+    user = User.new(default_user_attributes)
+    User.stub!(:authenticate).and_return(user)
+    create_user
     session[:user_id].should_not be_nil
     response.should be_redirect
   end
-  
+
   it 'fails login and does not redirect' do
-    post :create, :login => 'quentin', :password => 'bad password'
+    post :create, :login => 'bob', :password => 'bad password'
     session[:user_id].should be_nil
     response.should be_success
   end
 
   it 'logs out' do
-    login_as :quentin
+    login_as :bob
     get :destroy
     session[:user_id].should be_nil
     response.should be_redirect
   end
 
   it 'remembers me' do
-    post :create, :login => 'quentin', :password => 'test', :remember_me => "1"
+    post :create, :login => 'bob', :password => 'test', :remember_me => "1"
     response.cookies["auth_token"].should_not be_nil
   end
-  
+
   it 'does not remember me' do
-    post :create, :login => 'quentin', :password => 'test', :remember_me => "0"
+    post :create, :login => 'bob', :password => 'test', :remember_me => "0"
     response.cookies["auth_token"].should be_nil
   end
 
   it 'deletes token on logout' do
-    login_as :quentin
+    login_as :bob
     get :destroy
     response.cookies["auth_token"].should == []
   end
 
   it 'logs in with cookie' do
-    users(:quentin).remember_me
-    request.cookies["auth_token"] = cookie_for(:quentin)
+    users(:bob).remember_me
+    request.cookies["auth_token"] = cookie_for(:bob)
     get :new
     controller.send(:logged_in?).should be_true
   end
-  
+
   it 'fails expired cookie login' do
-    users(:quentin).remember_me
-    users(:quentin).update_attribute :remember_token_expires_at, 5.minutes.ago
-    request.cookies["auth_token"] = cookie_for(:quentin)
+    users(:bob).remember_me
+    users(:bob).update_attribute :remember_token_expires_at, 5.minutes.ago
+    request.cookies["auth_token"] = cookie_for(:bob)
     get :new
     controller.send(:logged_in?).should_not be_true
   end
-  
+
   it 'fails cookie login' do
-    users(:quentin).remember_me
+    users(:bob).remember_me
     request.cookies["auth_token"] = auth_token('invalid_auth_token')
     get :new
     controller.send(:logged_in?).should_not be_true
   end
 
+  protected
+
+  def create_user(opts={ })
+    post :create, default_user_attributes.merge(opts)
+  end
+
+  def default_user_attributes
+    { :login => 'bob', :password => 'valid_pass' }
+  end
+
   def auth_token(token)
     CGI::Cookie.new('name' => 'auth_token', 'value' => token)
   end
-    
+
   def cookie_for(user)
     auth_token users(user).remember_token
   end
