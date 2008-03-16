de4ef5c414c917707ce6d2078ac0ec985cd017a0
ed <ed@493adb5e-7a96-4b8c-87dc-e541e2512a3b>
2008-03-16 07:27:34 +0000

    don't use fixtures in user specs
    git-svn-id: svn+ssh://majakari.net/tampere.rb/trunk@183 493adb5e-7a96-4b8c-87dc-e541e2512a3b

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,107 +5,105 @@ require File.dirname(__FILE__) + '/../spec_helper'
 include AuthenticatedTestHelper
 
 describe User do
-  fixtures :users
-
-  describe 'being created' do
-    before do
-      @user = nil
-      @creating_user = lambda do
-        @user = create_user
-        violated "#{@user.errors.full_messages.to_sentence}" if @user.new_record?
-      end
-    end
-    
-    it 'increments User#count' do
-      @creating_user.should change(User, :count).by(1)
-    end
+  before(:each) do
+    @bob_password = 'mypass'
+    @bob = build_user(:login => 'bob',
+                      :password => @bob_password,
+                      :password_confirmation => @bob_password)
   end
 
   it 'requires login' do
     lambda do
-      u = create_user(:login => nil)
+      u = build_user(:login => nil)
       u.errors.on(:login).should_not be_nil
     end.should_not change(User, :count)
   end
 
   it 'requires password' do
     lambda do
-      u = create_user(:password => nil)
+      u = build_user(:password => nil)
       u.errors.on(:password).should_not be_nil
     end.should_not change(User, :count)
   end
 
   it 'requires password confirmation' do
     lambda do
-      u = create_user(:password_confirmation => nil)
+      u = build_user(:password_confirmation => nil)
       u.errors.on(:password_confirmation).should_not be_nil
     end.should_not change(User, :count)
   end
 
   it 'requires email' do
     lambda do
-      u = create_user(:email => nil)
+      u = build_user(:email => nil)
       u.errors.on(:email).should_not be_nil
     end.should_not change(User, :count)
   end
 
   it 'resets password' do
-    users(:quentin).update_attributes(:password => 'new password', :password_confirmation => 'new password')
-    User.authenticate('quentin', 'new password').should == users(:quentin)
+    @bob.update_attributes(:password => 'new password', :password_confirmation => 'new password')
+    User.authenticate('bob', 'new password').should == @bob
   end
 
-  it 'does not rehash password' do
-    users(:quentin).update_attributes(:login => 'quentin2')
-    User.authenticate('quentin2', 'test').should == users(:quentin)
+  it 'does not rehash password when authenticating' do
+    warn "hits database, use mocks/stubs instead"
+    @bob.update_attributes(:login => 'bob2')
+    User.authenticate('bob2', @bob_password).should == @bob
   end
 
   it 'authenticates user' do
-    User.authenticate('quentin', 'test').should == users(:quentin)
+    warn "hits database, use mocks/stubs instead"
+    @bob.save!
+    User.authenticate('bob', @bob_password).should == @bob
   end
 
   it 'sets remember token' do
-    users(:quentin).remember_me
-    users(:quentin).remember_token.should_not be_nil
-    users(:quentin).remember_token_expires_at.should_not be_nil
+    @bob.remember_me
+    @bob.remember_token.should_not be_nil
+    @bob.remember_token_expires_at.should_not be_nil
   end
 
   it 'unsets remember token' do
-    users(:quentin).remember_me
-    users(:quentin).remember_token.should_not be_nil
-    users(:quentin).forget_me
-    users(:quentin).remember_token.should be_nil
+    @bob.remember_me
+    @bob.remember_token.should_not be_nil
+    @bob.forget_me
+    @bob.remember_token.should be_nil
   end
 
   it 'remembers me for one week' do
     before = 1.week.from_now.utc
-    users(:quentin).remember_me_for 1.week
+    @bob.remember_me_for 1.week
     after = 1.week.from_now.utc
-    users(:quentin).remember_token.should_not be_nil
-    users(:quentin).remember_token_expires_at.should_not be_nil
-    users(:quentin).remember_token_expires_at.between?(before, after).should be_true
+    @bob.remember_token.should_not be_nil
+    @bob.remember_token_expires_at.should_not be_nil
+    @bob.remember_token_expires_at.between?(before, after).should be_true
   end
 
   it 'remembers me until one week' do
     time = 1.week.from_now.utc
-    users(:quentin).remember_me_until time
-    users(:quentin).remember_token.should_not be_nil
-    users(:quentin).remember_token_expires_at.should_not be_nil
-    users(:quentin).remember_token_expires_at.should == time
+    @bob.remember_me_until time
+    @bob.remember_token.should_not be_nil
+    @bob.remember_token_expires_at.should_not be_nil
+    @bob.remember_token_expires_at.should == time
   end
 
   it 'remembers me default two weeks' do
     before = 2.weeks.from_now.utc
-    users(:quentin).remember_me
+    @bob.remember_me
     after = 2.weeks.from_now.utc
-    users(:quentin).remember_token.should_not be_nil
-    users(:quentin).remember_token_expires_at.should_not be_nil
-    users(:quentin).remember_token_expires_at.between?(before, after).should be_true
+    @bob.remember_token.should_not be_nil
+    @bob.remember_token_expires_at.should_not be_nil
+    @bob.remember_token_expires_at.between?(before, after).should be_true
   end
 
 protected
-  def create_user(options = {})
-    record = User.new({ :login => 'quire', :email => 'quire@example.com', :password => 'quire', :password_confirmation => 'quire' }.merge(options))
-    record.save
-    record
+  def build_user(options = {})
+    returning User.new({ :login => 'user_login',
+                         :email => 'user@@example.com',
+                         :password => 'some_password',
+                         :password_confirmation => 'some_password'
+                       }.merge(options)) do |u|
+      u.valid? # called for side-effects of setting obj.errors
+    end
   end
 end
