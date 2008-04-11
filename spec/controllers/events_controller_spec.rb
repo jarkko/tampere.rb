e27975a4f93fb8c54283f36e913ee47229cdac0b
ed <ed@493adb5e-7a96-4b8c-87dc-e541e2512a3b>
2008-04-11 10:31:53 +0000

    use different contexts for logged in and guest users
    git-svn-id: svn+ssh://majakari.net/tampere.rb/trunk@233 493adb5e-7a96-4b8c-87dc-e541e2512a3b

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,48 +2,66 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe EventsController do
   include AuthenticatedTestHelper
-
-  describe "GET /events" do
-    it "should respond to index" do
+  
+  describe "all users" do
+    
+    it "should be able to get index" do
       get :index
       response.should be_success
     end
-  end
-
-  describe "GET /events/archived" do
-    it "should respond to index" do
+    
+    it "should be able to get archived messages" do
       evt = mock('event', :date => Time.now)
       Event.should_receive(:find_recent).with(100).and_return([evt])
       get :archived
       response.should be_success
     end
-  end
-
-  describe "GET /events/1" do
-    before(:each) do
+    
+    it "should be able to show an event" do
       @event = mock('event')
       Event.stub!(:find).and_return(@event)
-    end
-
-    it "should GET an event" do
+      
       get 'show', :id => 1
       #response.should be_success
-    end
-  end
+    end    
 
-  describe "GET /events/new" do
-    it "should redirect unregistered users to same page and  to new" do
+    it "should require registration when adding an event is attempted" do
       Event.should_receive(:new_default).never
       get 'new'
       flash[:notice].should_not be_nil
     end
+    
+  end # describe all users
 
-    it "should show new event form for registered users" do
+  describe "registered users" do
+    before(:each) do 
+      fake_login(uid=1)
+    end
+    
+    it "should be able to add new events" do
       Event.should_receive(:new_default).once
-      fake_login(1)
+      
       get 'new'
       flash[:notice].should be_nil
       response.should be_success
     end
-  end
+  
+    it "should be able to edit an existing event" do
+      evt = mock('event', 
+                 :id => 1,
+                 :description => 'desc', :date => Date.today,
+                 :location_id => 1)
+      Event.stub!(:find).and_return(evt)
+      fake_login(1)
+      get :edit, :id => 1
+      response.should be_success
+    end    
+    
+    xit "should be able to update an event" do
+      evt = mock('event', :id => 1, :description => 'desc',
+                 :date => Date.today, :location_id => 1)
+      put :update, { :id => 1, :description => 'modified' }
+      flash[:notice].should_not be_nil
+    end
+  end # registered users
 end
