ee91f73aa6e3513089a7f6efdb2ccedd52966502
ed <ed@493adb5e-7a96-4b8c-87dc-e541e2512a3b>
2008-05-19 10:45:52 +0000

    updated about page
    git-svn-id: svn+ssh://majakari.net/tampere.rb/trunk@285 493adb5e-7a96-4b8c-87dc-e541e2512a3b

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :users
+  map.connect 'about', :controller => 'about', :action => 'show'
 
   map.resource :session
-
+  
+  map.resources :users
   map.resources :locations
   map.resources :events, :collection => { :archived => :get }
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
